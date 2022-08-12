8c3b3b1e235ed9a9bbf9251ac7ac5ec5fbaeb4d1
Strift <lau.cazanove@gmail.com>
2022-08-12 14:44:25 +0200

    Change default region

--- plugins/contentstack.js
+++ plugins/contentstack.js
@@ -7,9 +7,7 @@ const Stack = contentstack.Stack({
   api_key: process.env.CONTENTSTACK_API_KEY,
   delivery_token: process.env.CONTENTSTACK_DELIVERY_TOKEN,
   environment: process.env.CONTENTSTACK_ENVIRONMENT,
-  region: process.env.CONTENTSTACK_REGION
-    ? process.env.CONTENTSTACK_REGION
-    : 'us',
+  region: contentstack.Region.EU,
   live_preview: {
     management_token: process.env.CONTENTSTACK_MANAGEMENT_TOKEN
       ? process.env.CONTENTSTACK_MANAGEMENT_TOKEN
